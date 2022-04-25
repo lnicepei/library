b9b45039c7aa8c35f1815903473244dbc96366e0
lnicepei <dmitrii.shaidorov@gmail.com>
2022-04-25 21:21:59 +0300

    Make getting data from user's input availible

--- script.js
+++ script.js
@@ -1,34 +1,39 @@
-let myLibrary = [];
-
-function Book(name, author){
-
-    //the constructor
+function Book(name, author, ifread){
     this.name = name;
     this.author = author;
+    this.ifread = ifread;
+}
+let myLibrary = [];
+
+document.querySelector(".add-button").addEventListener("click", displayInputField);
+document.querySelector(".remove-button").addEventListener("click", removeInputField);
+document.querySelector(".submit-button").addEventListener("click", addBookToLibrary);
+
+function addBookToLibrary(e){
+    //do stuff here
+    getDataFromForm();
+    document.querySelector(".book-container").textContent = "";
+    for(let book in myLibrary){
+        let newBook = document.createElement("div");
+        newBook.className = "book";
+        document.querySelector(".book-container").appendChild(newBook);
+        newBook.textContent += myLibrary[book].name + " " + myLibrary[book].author + " " + myLibrary[book].ifread;
+    }
+    removeInputField();
 }
 
-document.querySelector(".add-button").addEventListener("click", displayInputField)
-document.querySelector(".remove-button").addEventListener("click", removeInputField)
-function addBookToLibrary(e){
-
-    displayInputField();
-    //do stuff here
-    const newBook = new Book("name", "author");
+function getDataFromForm(){
+    let name = document.getElementById("bookname").value;
+    let author = document.getElementById("author").value;
+    let ifread = document.getElementById("ifread").checked;
+    const newBook = new Book(name, author, ifread);
     myLibrary.push(newBook);
-    for(let book in myLibrary){
-        document.querySelector(".book-container").textContent += book;
-    }
 }
 
 function displayInputField(e){
-    // if (document.querySelector(".input-field").style.display !== "none") {
-    //     document.querySelector(".input-field").style.display = "none";
-    // } else {
-    //     document.querySelector(".input-field").style.display = "block";
-    // }
-    document.querySelector(".input-field").style.display = "flex" 
+    document.getElementById("myForm").style.display = "block";
 }
+
 function removeInputField(e){
-    if(document.querySelector(".input-field").style.display != "none")
-    document.querySelector(".input-field").style.display = "none" 
+    document.getElementById("myForm").style.display = "none";
 }
