d4f48096571ead0304b140e49a9e9d5416c9a588
lnicepei <dmitrii.shaidorov@gmail.com>
2022-04-26 17:03:20 +0300

    Rewrite event listeners and create book attributes

--- script.js
+++ script.js
@@ -8,45 +8,73 @@ let myLibrary = [];
 
 document.querySelector(".add-button").addEventListener("click", displayInputField);
 document.querySelector(".remove-button").addEventListener("click", removeInputField);
-// document.querySelector(".submit-button").addEventListener("click", addBookToLibrary);
+document.querySelector("form").addEventListener("submit", addBookToLibrary);
 
 function addBookToLibrary(e){
-    //do stuff here
-    getDataFromForm();
+    e.preventDefault();
+    pushDataFromForm();
+
     document.querySelector(".book-container").textContent = "";
-    for(let book in myLibrary){
+
+    for (let book in myLibrary){
         let newBook = document.createElement("div");
         newBook.className = "book";
-        newBook.textContent = myLibrary[book].author + myLibrary[book].pageNumber + myLibrary[book].ifWasRead;  
-        // let author = newBook.createElement("div");
-        // let pageNumber = newBook.createElement("div");
-        // let ifWasRead = newBook.createElement("div");
-        document.querySelector(".book-container").appendChild(newBook);
-        // document.querySelector(".book").appendChild("sdfsdg");
-        // author.appendChild(myLibrary[book].author);
-        // pageNumber.appendChild(myLibrary[book].pageNumber);
-        // ifWasRead.appendChild(myLibrary[book].pageNumber);
+
         let name = document.createElement("div");
-        document.querySelector(".book").appendChild(name);
         name.textContent = myLibrary[book].name;
+        name.className = "name";
+
+        let author = document.createElement("div");
+        author.textContent = myLibrary[book].author;
+        author.className = "author";
+
+        let pageNumber = document.createElement("div");
+        pageNumber.textContent = myLibrary[book].pageNumber;
+        pageNumber.className = "pageNumber";
+
+        let ifWasRead = document.createElement("div");
+        ifWasRead.textContent = myLibrary[book].ifWasRead;
+        ifWasRead.className = "ifWasRead";
+
+        let removeButton = document.createElement("button");
+        removeButton.textContent = "Delete";
+        removeButton.addEventListener("click", removeBook);
+
+        document.querySelector(".book-container").appendChild(newBook);
+
         newBook.appendChild(name);
+        newBook.appendChild(author);
+        newBook.appendChild(pageNumber);
+        newBook.appendChild(ifWasRead);
+        newBook.appendChild(removeButton);
     }
     removeInputField();
 }
 
-function getDataFromForm(){
+function pushDataFromForm(){
     let name = document.getElementById("bookname").value;
     let author = document.getElementById("author").value;
     let pageNumber = document.getElementById("pages").value;
     let ifWasRead = document.getElementById("ifWasRead").checked;
+
     const newBook = new Book(name, author, pageNumber, ifWasRead);
+
     myLibrary.push(newBook);
 }
 
-function displayInputField(e){
+function displayInputField(){
     document.getElementById("myForm").style.display = "block";
 }
 
-function removeInputField(e){
+function removeInputField(){
     document.getElementById("myForm").style.display = "none";
+
+    document.getElementById("bookname").value = "";
+    document.getElementById("author").value = "";
+    document.getElementById("pages").value = "";
+    document.getElementById("ifWasRead").checked = "";
+}
+
+function removeBook(e){
+    
 }
